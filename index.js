787d0eb1d99f0e4352056eb06e9ad327a01f1bfc
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-02-24 12:03:56 +0600

    delete with edit blog implement

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app=express();
 const port =process.env.PORT || 5000;
 const cors = require ('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 
-app.use(express());
+app.use(express.json());
 app.use(cors());
 
 
@@ -29,6 +30,7 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const blogsCollection = client.db("balaj_Consulting").collection("blogs");
+    const usersCollection = client.db("balaj_Consulting").collection("users");
 
 
     app.get('/blogs',async(req,res)=>{
@@ -36,13 +38,102 @@ async function run() {
         res.send(result);
         console.log('result,',result)
     })
+    app.post ('/blogs',async(req,res)=>{
+           const blog=req.body;
+           const result =await blogsCollection.insertOne(blog);
+           res.send(result)
+           console.log(result)
+       })
+
+       // delete id ways blogs
+      //  app.delete('/books/:id',async(req,res)=>{
+      //   const id = req.params.id;
+      //       const query = { _id: new ObjectId(id)};
+      //       const result = await booksCollections.deleteOne(query);
+      //     console.log(result)
+      //       res.send(result);
+      //  })
+         
+
+      app.delete('/blogs/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result =await blogsCollection.deleteOne(query);
+        console.log('dlete..',result);
+        res.send(result)
+      })
+    
+
+      /// blog id ways Edit ..
+      // image,title,subtitle, authorImage, authorName,profession
+      app.put('/blogs/:id', async (req, res) => {
+        const id = req.params.id;
+        const body = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+            $set: {
+                image: body.image,
+                title: body.title,
+                subtitle: body.subtitle,
+                authorImage: body.authorImage,
+                authorName: body.authorName,
+                profession: body.profession,
+            },
+        };
+        const result = await blogsCollection.updateOne(filter, updateDoc);
+        console.log(result)
+        res.send(result);
+    });
+      
+    app.get('/users',async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+      console.log('result,',result)
+  })
+
+  // app.post('/jwt',(req,res)=>{
+  //   const user=req.body;
+  //   const token =jwt.sign(user,process.env.ACCSS_TOKEN_SECRET,{expiresIn:'1h'});
+  //   res.send({token})
+  // })
+
+    // app.post('/users', async (req, res) => {
+
+    //   const user = req.body;
+    //   console.log(user);
+    //   const query = { email: user.email };
+    //   const existingUser = await usersCollection.findOne(query);
+    //   console.log('existing User', existingUser);
+    //   if (existingUser) {
+    //     return res.send({ message: 'User already exists' });
+    //   }
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    
+    
+    // });
+     
  
+   
 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('user..',user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existing user', existingUser);
+      if (existingUser) {
+        return res.send({ Message: 'user already exists' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-
-
-
-
+//  app.post ('/users',async(req,res)=>{
+//      const user=req.body;
+//      const result =await usersCollection.insertOne(user);
+//      res.send(resu)
+//  })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
